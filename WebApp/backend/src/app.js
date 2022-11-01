137894f0884fa776c79d37d5a173a8310b12311c
Dineth Dissanayake <77220940+Dineth-Dissanayake@users.noreply.github.com>
2022-11-01 10:05:18 +0530

    INITIAL_FILES

--- WebApp/backend/src/app.js
+++ WebApp/backend/src/app.js
@@ -1,1 +1,27 @@
-console.log("Helloow World");
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import {connect} from './utils/db.connection';
+
+//const auth = require("./api/middleware/pw.auth");
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+//IMPORT ROUTES
+// const bookRoutes = require("./api/routes/book.r");
+// app.use(bookRoutes);
+
+// app.use("/api/", require("./api/routes/librarian.auth.r"));
+
+app.listen(PORT, () => {
+    console.log('🚀 SERVER IS UP & RUNNING ON:', PORT);
+    connect();
+});
